fff4deb816ee0f3473c614bdfa1d33621e0170c8
wilboron <williammingardi@gmail.com>
2020-09-13 14:29:22 -0300

    Changes validates_with to format in User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true
-  validates_format_of :email, with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validate :email, format: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :name, :email, presence: true
   validates :name, length: { in: 2..50 }
   validates :password, length: { in: 5..40 }
